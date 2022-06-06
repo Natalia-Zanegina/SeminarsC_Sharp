c544e342adc0c9ead89f7779e32d1f4a05926245
NataliaZanegina <nataliazanegina@yandex.ru>
2022-06-06 11:42:15 +0300

    First seminar completed!

---
+++ Seminar1/Program.cs
@@ -0,0 +1,12 @@
+﻿int num, current;
+
+Console.Write("Input integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+current = num * (-1);
+
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+} 

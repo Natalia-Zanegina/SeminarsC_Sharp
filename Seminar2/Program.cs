427ee14d566b73159576aa58a33c84632781653c
NataliaZanegina <nataliazanegina@yandex.ru>
2022-06-11 22:11:45 +0300

    Second seminar completed!

---
+++ Seminar2/Program.cs
@@ -0,0 +1,38 @@
+﻿
+/* int FindMaxPart(int num)
+{
+    int dec, ed;
+    if(num >= 10 && num <= 99)
+    {
+        dec = num /10;
+        ed = num % 10;
+
+        if(dec > ed)
+        {
+            return dec;
+        }
+        else
+        {
+            return ed;
+        }
+    }
+    else
+    {
+            return -1;
+    }
+}
+
+// c = a % b
+Console.Write("Input two-digit number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int result = FindMaxPart(a);
+
+if(result == -1)
+{
+    Console.WriteLine("Your number is not a two-digit!");
+}
+else
+{
+Console.WriteLine("Bigger number of " + a + " is " + result);
+} */
